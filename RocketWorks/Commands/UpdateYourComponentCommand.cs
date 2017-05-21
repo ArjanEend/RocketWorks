4f06c0c318a8c04af9e9468451ab98cb8561adea
Arjan Cordia <arjaneend@gmail.com>
2017-05-21 17:31:23 +0200

    ADD new command

---
+++ RocketWorks/Commands/UpdateYourComponentCommand.cs
@@ -0,0 +1,32 @@
+﻿using RocketWorks.Entities;
+using RocketWorks.Pooling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RocketWorks.Commands
+{
+    [Serializable]
+    public partial class UpdateYourComponentCommand<T> : NetworkCommandBase<T> where T : EntityContext
+    {
+        protected IComponent component;
+        protected uint hash;
+
+        public UpdateYourComponentCommand() { }
+
+        public UpdateYourComponentCommand(IComponent component, uint hash)
+        {
+            this.component = component;
+            this.hash = hash;
+        }
+
+        public override void Execute(T target, int uid)
+        {
+            lock (component)
+            {
+                target.Pool.ReplaceComponent(component, hash, -1);
+            }
+        }
+    }
+}
